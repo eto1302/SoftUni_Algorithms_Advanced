7de6dd7ce0febe003fb3c9537cf62f9d045b22e5
eto1302 <martinangelov1302@gmail.com>
2021-01-20 18:49:23 +0200

    This is the Graph(Bellman-Ford and Longest Path in DAG) Lab from the SoftUni course Algorithms Advanced

---
+++ 01_GraphsBellmanFordAndLongestPathInDag/Bellman-Ford/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Bellman_Ford
+{
+    class Edge
+    {
+        public int from { get; set; }
+        public int to { get; set; }
+        public int weight { get; set; }
+    }
+    class Program
+    {
+        public static int n, e;
+        public static List<Edge> edges;
+        public static int source, destination;
+        public static int[] d, prev;
+        public static bool containsNegativeCycle;
+        static void Main(string[] args)
+        {
+            Input();
+            InitializeStructures();
+            BellmanFordAlgorithm();
+            Output();
+        }
+
+        private static void Output()
+        {
+            if (containsNegativeCycle)
+            {
+                Console.WriteLine("Negative Cycle Detected");
+                return;
+            }
+            Console.WriteLine(GetPath(destination));
+            Console.WriteLine(d[destination - 1]);
+        }
+
+        private static string GetPath(int i)
+        {
+            List<int> path = new List<int>();
+            while (i != source - 1)
+            {
+                path.Add(i);
+                i = prev[i - 1];
+            }
+            path.Reverse();
+            return string.Join(" ", path);
+        }
+
+        private static void BellmanFordAlgorithm()
+        {
+            for (int i = 0; i < n; ++i)
+            {
+                bool hasChanged = false;
+                for (int j = 0; j < n; ++j)
+                {
+                    if (d[j] == int.MaxValue) continue;
+                    foreach (var edge in edges.Where(e => e.from - 1 == j).ToArray())
+                    {
+                        if (d[j] + edge.weight < d[edge.to - 1])
+                        {
+                            d[edge.to - 1] = d[j] + edge.weight;
+                            prev[edge.to - 1] = j + 1;
+                            hasChanged = true;
+                        }
+                    }
+                }
+                if (!hasChanged) break;
+            }
+
+            foreach (var edge in edges)
+            {
+                if (d[edge.to - 1] > d[edge.from - 1] + edge.weight)
+                {
+                    containsNegativeCycle = true;
+                    return;
+                }
+            }
+        }
+
+        private static void InitializeStructures()
+        {
+            d = new int[n];
+            prev = new int[n];
+            d = d.Select(e => e = int.MaxValue).ToArray();
+            d[source - 1] = 0;
+        }
+
+        private static void Input()
+        {
+            n = int.Parse(Console.ReadLine());
+            e = int.Parse(Console.ReadLine());
+            edges = new List<Edge>();
+            for (int i = 0; i < e; i++)
+            {
+                var inputParameters = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                edges.Add(new Edge { from = inputParameters[0], to = inputParameters[1], weight = inputParameters[2] });
+            }
+
+            source = int.Parse(Console.ReadLine());
+            destination = int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ 01_GraphsBellmanFordAndLongestPathInDag/LongestPath/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Bellman_Ford
+{
+    class Edge
+    {
+        public int from { get; set; }
+        public int to { get; set; }
+        public int weight { get; set; }
+    }
+    class Program
+    {
+        public static int n, e;
+        public static List<Edge> edges;
+        public static int source, destination;
+        public static int[] d;
+        public static Stack<int> topologicallySortedVertices;
+        static void Main(string[] args)
+        {
+            Input();
+            InitializeStructures();
+            TopologicalSort();
+            FindLongestPath();
+            Output();
+        }
+
+        private static void FindLongestPath()
+        {
+            while (topologicallySortedVertices.Count != 0)
+            {
+                int currentVertex = topologicallySortedVertices.Pop();
+                foreach (var edge in edges.Where(e => e.from == currentVertex).ToArray())
+                {
+                    if (d[edge.to - 1] < d[edge.from - 1] + edge.weight)
+                    {
+                        d[edge.to - 1] = d[edge.from - 1] + edge.weight;
+                    }
+                }
+            }
+
+
+        }
+
+        private static void Output()
+        {
+            Console.WriteLine(d[destination - 1]);
+        }
+        private static void TopologicalSort()
+        {
+            for (int i = 1; i < n + 1; i++)
+            {
+                TraverseChildren(i);
+            }
+        }
+
+        private static void TraverseChildren(int i)
+        {
+            foreach (var edge in edges.Where(e => e.from == i).ToArray())
+            {
+                TraverseChildren(edge.to);
+            }
+            if (!topologicallySortedVertices.Contains(i)) topologicallySortedVertices.Push(i);
+        }
+
+        private static void InitializeStructures()
+        {
+            d = new int[n];
+            topologicallySortedVertices = new Stack<int>();
+            d = d.Select(e => e = int.MinValue).ToArray();
+            d[source - 1] = 0;
+        }
+
+        private static void Input()
+        {
+            n = int.Parse(Console.ReadLine());
+            e = int.Parse(Console.ReadLine());
+            edges = new List<Edge>();
+            for (int i = 0; i < e; i++)
+            {
+                var inputParameters = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                edges.Add(new Edge { from = inputParameters[0], to = inputParameters[1], weight = inputParameters[2] });
+            }
+
+            source = int.Parse(Console.ReadLine());
+            destination = int.Parse(Console.ReadLine());
+        }
+    }
+}
